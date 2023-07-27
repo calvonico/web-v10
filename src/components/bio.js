03bd6fc6a03811c98d09a6178a02a9df02d6bf63
Nico Calvo <calvonico@users.noreply.github.com>
2023-07-27 16:37:43 +0100

    actualizada Bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -19,12 +19,16 @@ export default function Bio() {
           Hey, I'm <strong>Nico</strong> 👋
         </h2>
         <p className="mt-1">
-          I'm a entrepeneur, designer, and frontend developer. Born and raised
-          in Buenos Aires, Argentina, now based in beautiful Oporto, Portugal.
+          I'm a entrepeneur, designer and developer passionate about building things that help people. 
         </p>
         <p className="mt-1">
+        Born and raised in Buenos Aires, Argentina, now based in beautiful Oporto, Portugal.
         I like to travel and explore new cities; and I love watching movies, football and video games.
         </p>
+        <div className="blobs-container available mt-4">
+          <div className='blob green'></div>
+          <div className="text-sm text-gray-500">Available for freelance work</div>
+        </div>
       </div>
     </div>
   );
