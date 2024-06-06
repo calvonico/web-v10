950991c98758442dfb15de3beec0f88491e8989f
Nico Calvo <calvonico@users.noreply.github.com>
2024-06-06 15:09:52 +0100

    actualizada bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -22,7 +22,7 @@ export default function Bio() {
           I'm a entrepeneur, designer and developer passionate about building things that help people. Born and raised in Buenos Aires, Argentina, now based in beautiful Porto, Portugal.
         </p>
         <p className="mt-3">
-          For the last 12 years my work has consisted of marketing design: building creative assets for email, social media, websites, and more. I have also developed several websites, UI for mobile applications, and have mentored and led teams of designers.
+          For the last 14 years my work has consisted of marketing design: building creative assets for email, social media, websites, and more.
         </p>
         <p className="mt-3">
           Lately I've been working as a Creative Lead, coordinating a group of designers focusing on achieving their best version to create unique and quality images and illustrations for our clients.
@@ -31,8 +31,8 @@ export default function Bio() {
           I like to travel and explore new cities; and I love watching movies, football and video games.
         </p>
         <div className="blobs-container available mt-4">
-          <div className='blob red'></div>
-          <div className="text-sm text-gray-500">Not Available for freelance work</div>
+          <div className='blob green'></div>
+          <div className="text-sm text-gray-500">Building</div>
         </div>
       </div>
     </div>
